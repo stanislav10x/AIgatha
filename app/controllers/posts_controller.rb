fc5bbaa9ea0df888c9f7b46debee4ec6dd24e1a9
stanislav10x <56184122+stanislav10x@users.noreply.github.com>
2023-12-06 23:28:19 +0000

    updated posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,42 +13,8 @@ class PostsController < ApplicationController
     else
       redirect_to("/games/#{the_post.game_id}", { :alert => the_post.errors.full_messages.to_sentence })
     end
-
-    #api_posts_array = Array.new
-
-
-
-    #client = OpenAI::Client.new
-    #@response = client.chat(
-     # parameters: {
-      #  model: "gpt-3.5-turbo",
-      #  messages: [{ role: "user", content: "Hello!"}],
-      #  temperature: 0.7,
-      #},
-    #)
-     # response.fetch("choices").at(0).fetch("message").fetch("content")
-
-
-
-
-
   end
 
-  #def update
-  #  the_id = params.fetch("path_id")
-  #  the_post = Post.where({ :id => the_id }).at(0)
-
-  #  the_post.game_id = params.fetch("query_game_id")
-  #  the_post.gpt_created = params.fetch("query_gpt_created", false)
-  #  the_post.body = params.fetch("query_body")
-
-  #  if the_post.valid?
-  #    the_post.save
-  #    redirect_to("/posts/#{the_post.id}", { :notice => "Post updated successfully."} )
-  #  else
-  #    redirect_to("/posts/#{the_post.id}", { :alert => the_post.errors.full_messages.to_sentence })
-  #  end
-  #end
 
   def destroy
     the_id = params.fetch("path_id")
@@ -58,4 +24,4 @@ class PostsController < ApplicationController
 
     redirect_to("/posts", { :notice => "Post deleted successfully."} )
   end
-e
+end
