4f491496ed5f242ae5a36d5b745093fbd77dea81
stanislav10x <56184122+stanislav10x@users.noreply.github.com>
2023-12-07 00:18:53 +0000

    issue with status

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -56,6 +56,7 @@ class GamesController < ApplicationController
     the_game = Game.new
     the_game.author_id = current_user.id
     the_game.title = params.fetch("query_title")
+    the_game.status = "in progress"
 
     setting_options = ["old mansion", "spaceship", "art museum", "18th century galleon", "underwater metropolis", "neo-noir", "parallel universe", "luxurious island hotel", "AI-run megacity"]
 
